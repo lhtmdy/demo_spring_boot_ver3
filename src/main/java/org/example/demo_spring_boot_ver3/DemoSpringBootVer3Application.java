f1cfee7c4c733c0a0c9b4add3e5e2bfe281438b5
IEAD\2312004 <mandy.li@iisigroup.com>
2026-01-14 11:27:20 +0800

    first commit

---
+++ src/main/java/org/example/demo_spring_boot_ver3/DemoSpringBootVer3Application.java
@@ -0,0 +1,13 @@
+package org.example.demo_spring_boot_ver3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootVer3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootVer3Application.class, args);
+	}
+
+}
